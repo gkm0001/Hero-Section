eddf6f2c1eb313d54514c459b85ba98a36e000c9
GOVIND Mishra <professionalgkm@gmail.com>
2025-04-28 21:43:28 +0530

    Remove the unused code

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,13 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
-import Header from './components/NavBar'
 import HeroSection from './components/HeroSection'
-import HeroBox from './components/HeroBox'
+
 
 function App() {
 
 
   return (
     <>
-      {/* <Header/> */}
       <HeroSection/>
-      {/* <HeroBox/> */}
     </>
   )
 }
